365ff31c2c621210fd7c6d8d45cc481affa8a0b2
pak9511 <56289238+pak9511@users.noreply.github.com>
2021-07-23 18:07:01 +0900

    Create 1 Two Sum.java

---
+++ Easy/1 Two Sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        Map<Integer,  Integer> map = new HashMap<Integer,  Integer>();  
+        int[] res= new int [2];
+        
+        for (int i=0; i<nums.length ;i++) {
+            map.put(nums[i],i);
+        }
+        
+        for (int i=0; i<nums.length; i++){
+            if (map.containsKey(target-nums[i]) && i != map.get(target-nums[i])){
+                res[0]=i;
+                res[1]=map.get(target-nums[i]);
+                return res; 
+            } 
+        }
+    return res;
+    }
+}
